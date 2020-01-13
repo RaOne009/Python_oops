20d250f27016119cc3648c6c4142ff1c0c73aeac
Rohit Sharma <rs1257933@gmail.com>
2020-01-13 16:02:49 +0530

    1st

--- emp.py
+++ emp.py
@@ -7,8 +7,8 @@ class Employee:
         self.income = income
 #todo nothing use pass (kush ni)
     pass
-tony = Employee("tony", "stark", "40000")
-rohit = Employee("rohit", "sharma", "50000")
+tony = Employee('Tony', 'stark', 400000)
+rohit = Employee('Rohit', 'sharma', 500000)
 
 # tony.fname = "tony"
 # tony.lname = "stark"
@@ -17,6 +17,6 @@ rohit = Employee("rohit", "sharma", "50000")
 # rohit.fname = "rohit"
 # rohit.lname = "sharma"
 # rohit.income = "40000"
-#
+
  print(tony.income)
  print(rohit.fname, tony.lname)
